1c778f036097306ba191e8aed7f1d8e46619327d
Jiaxu Su <sujiaxushanghai@gmail.com>
2022-02-09 12:08:44 -0500

    first

---
+++ core/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+
+class SuperUser(UserAdmin):
+    ordering = ['id']
+
+
+admin.site.register(User, SuperUser)
